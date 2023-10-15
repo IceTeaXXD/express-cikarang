e97b3234b9fb3ac4a1c9293307e7325832216ab4
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-10-15 18:53:56 +0700

    feat: mongoose db connection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ipModel = require("./ip.model");
 var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
@@ -6,6 +7,17 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.get("/", (req, res) => res.type('html').send(html));
 app.get("/location", (req, res) => res.json({ location: "cikarang" }));
+app.get("/visitor", async (req, res) => {
+  let ipData = new ipModel({
+    ip_addr: req.ip,
+    date_added: new Date(),
+  });
+  await ipData.save();
+
+  let numVisits = await ipModel.count()
+
+  return res.json({ your_ip: req.ip, visitor_number: numVisits });
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
